dac04e39fcf9383bce7c3d4a986500744629f62d
CicaMatt <matteocicalese01@gmail.com>
2022-04-30 20:04:40 +0200

    Added decision tree, naive bayes, random forest and knn classifiers

--- decision_tree.py
+++ decision_tree.py
@@ -4,10 +4,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from scipy.stats import zscore
 from sklearn.preprocessing import MinMaxScaler
-from sklearn import metrics
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.callbacks import EarlyStopping
+from sklearn import metrics, tree
 
 
 # reading CSV files based on given 'set' and instantiate a DataFrame
@@ -93,9 +90,7 @@ training_data = data.values
 
 # labels for training
 labels = labels_full.values
-print(labels)
 
-###KERAS MODEL
 
 # VALIDATION
 
@@ -103,48 +98,11 @@ print(labels)
 print("Validation - 80/20 split")
 x_training, x_testing, y_training, y_testing = train_test_split(training_data, labels, test_size=0.20, random_state=42)
 
+model = tree.DecisionTreeClassifier(criterion="entropy", random_state=100, min_samples_leaf=5)
+# model = tree.ExtraTreeClassifier(criterion="gini", min_samples_leaf=5)
 
-# create and fit model
-model = Sequential()
-# il metodo add aggiunge layer alla rete neurale
-# Dense é un tipo di layer, dove il primo valore rappresenta il numero di neuroni
-# input_dim che costituisce il numero di colonne di training_data
-# activation rappresenta la funzione di attivazione utilizzata
-# RELU é una funzione lineare che da in output l'input diretto se esso é positivo, zero altrimenti
-model.add(Dense(10, input_dim=training_data.shape[1], activation='relu'))
-model.add(Dense(40, input_dim=training_data.shape[1], activation='relu'))
-model.add(Dense(10, input_dim=training_data.shape[1], activation='relu'))
-# kernel_initializer rappresenta l'inizializzatore della matrice dei pesi del kernel
-model.add(Dense(1, kernel_initializer='normal'))
-# softmax é una funzione di attivazione che converte un vettore di numeri in un vettore di probabilitá, dove
-# i valori di probabilitá sono proporzionali alla relativa scala di ogni valore nel vettore
-model.add(Dense(labels.shape[1], activation='softmax'))
-
-# compile serve a configurare il modello per l'addestramento
-# loss rappresenta la funzione di perdita, optimizer rappresenta l'istanza dell'optimizer
-# categorical_crossentropy é usata come funzione di perdita per la classificazione multiclasse
-# quando ci sono due o piú label in output
-# adam é un metodo di stocastica gradiente di discesa basato sulla stima adattiva dei momenti di primo e secondo ordine
-model.compile(loss='categorical_crossentropy', optimizer='adam')
-
-# si istanzia un monitor che é in grado di fermare l'addestramento quando una certa metrica ha smesso di migliorare
-# monitor rappresenta la quantitá da monitorare
-# min_delta rappresenta il minimo cambiamento nella quantitá monitorata che va qualificata come miglioramento
-# patience rappresenta il numero di epoche senza miglioramento significativo dopo le quali l'addestramento sará fermato
-# verbose=1 serve a stampare messaggi di debug
-# mode rappresenta la modalitá di monitoraggio della quantitá in esame
-monitor = EarlyStopping(monitor='val_loss', min_delta=1e-3, patience=5, verbose=1, mode='auto')
-
-# fit addestra il modello per un certo numero di epoche (ovvero iterazioni sul dataset)
-# primo e secondo parametro sono rispettivamente dati di input e dati obiettivo
-# validation_data rappresenta i dati sui quali valutare la perdita e le metriche di ogni modello alla fine di ogni epoca
-# callback contiene la lista di instanze di callback
-# verbose = 2 mostra una riga di info per ogni epoca
-# epochs rappresenta il numero di epoche per cui va addestrato il modello
-# TRAINING
 start = time.time()
-model.fit(x_training, y_training, validation_data=(x_testing, y_testing),
-          callbacks=[monitor], verbose=2, epochs=100)
+model.fit(x_training, y_training)
 print("\nTraining time: " + str(time.time() - start)[0:7] + "s")
 
 # PREDICTION
