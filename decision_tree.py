bug fixed in metrics.py, added svm classificator and feature scaling code

decision_tree.py
@@ -1,12 +1,8 @@
 import time
 import numpy as np
 import pandas as pd
-import seaborn
-from matplotlib import pyplot as plt
-from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.model_selection import train_test_split
-from scipy.stats import zscore
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn import metrics, tree
 from utils.data_setup import DataSetup
 from utils.metrics import Metrics
@@ -37,9 +33,8 @@ data = data.drop(columns='type')
 
 # Feature Scaling - Z-Score Normalization
 print("Scaling training set features")
-# for i in data.columns:
-#     data[i] = zscore(data[i])
-data = MinMaxScaler().fit_transform(data)
+data = StandardScaler().fit_transform(data)
+# data = MinMaxScaler().fit_transform(data)
 data = pd.DataFrame(data)
 
 # .values trasforma i dati in formato tabellare DataFrame in un array multidimensionale NumPy

k_neighbours.py
@@ -2,14 +2,15 @@ import time
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from scipy.stats import zscore
 from sklearn.preprocessing import MinMaxScaler
 from sklearn import neighbors
 from utils.data_setup import DataSetup
 from utils.metrics import Metrics
+from sklearn.feature_selection import SelectKBest, chi2
+
 
 # Data retrieving
-data = DataSetup.data_setup(5)
+data = DataSetup.data_setup(1)
 
 # Data preparation
 # Removing all duplicates
@@ -48,6 +49,8 @@ training_data = data.values
 
 # labels for training
 labels = labels_full.values
+
+data = SelectKBest(chi2, k=50).fit_transform(training_data, labels)
 
 
 # VALIDATION

keras_neural_network.py
@@ -2,9 +2,8 @@ import time
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from scipy.stats import zscore
-from sklearn.preprocessing import MinMaxScaler
-from sklearn import metrics
+from sklearn.feature_selection import SelectKBest, chi2, f_classif
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.callbacks import EarlyStopping
@@ -12,7 +11,7 @@ from utils.data_setup import DataSetup
 from utils.metrics import Metrics
 
 # Data retrieving
-data = DataSetup.data_setup(5)
+data = DataSetup.data_setup(1)
 
 # Removing all duplicates
 data = data.drop_duplicates()
@@ -38,12 +37,9 @@ data = data.drop(columns='type')
 
 # Feature Scaling - Z-Score Normalization
 print("Scaling training set features")
-for i in data.columns:
-    data[i] = zscore(data[i])
-# data = MinMaxScaler().fit_transform(data)
-# data = pd.DataFrame(data)
-
-
+# data = StandardScaler().fit_transform(data)
+data = MinMaxScaler().fit_transform(data)
+data = pd.DataFrame(data)
 
 # .values trasforma i dati in formato tabellare DataFrame in un array multidimensionale NumPy
 # training data for the neural net
@@ -51,6 +47,9 @@ training_data = data.values
 
 # labels for training
 labels = labels_full.values
+
+# data = SelectKBest(chi2, k=50).fit_transform(training_data, labels)
+# print(data.shape[1])
 
 ###KERAS MODEL
 

random_forest.py
@@ -2,9 +2,8 @@ import time
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from scipy.stats import zscore
-from sklearn.preprocessing import MinMaxScaler
-from sklearn import metrics, ensemble
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn import ensemble
 from utils.data_setup import DataSetup
 from utils.metrics import Metrics
 
@@ -35,11 +34,9 @@ data = data.drop(columns='type')
 
 # Feature Scaling - Z-Score Normalization
 print("Scaling training set features")
-for i in data.columns:
-    data[i] = zscore(data[i])
-# MinMaxScaler().fit_transform(data_st)
-# print(data_st.head(50))
-# print(data_st.tail(50))
+data = StandardScaler().fit_transform(data)
+# data = MinMaxScaler().fit_transform(data)
+data = pd.DataFrame(data)
 
 
 # .values trasforma i dati in formato tabellare DataFrame in un array multidimensionale NumPy

second_neural_network.py
@@ -2,9 +2,7 @@ import time
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from scipy.stats import zscore
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn import metrics
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.callbacks import EarlyStopping
@@ -38,11 +36,9 @@ data = data.drop(columns='type')
 
 # Feature Scaling - Z-Score Normalization
 print("Scaling training set features")
-for i in data.columns:
-    data[i] = zscore(data[i])
-# MinMaxScaler().fit_transform(data_st)
-# print(data_st.head(50))
-# print(data_st.tail(50))
+data = StandardScaler().fit_transform(data)
+# data = MinMaxScaler().fit_transform(data)
+data = pd.DataFrame(data)
 
 
 # .values restituisce una rappresentazione formato Numpy di un DataFrame, in altre parole trasforma i dati in formato tabellare in un array multidimensionale

svm.py
@@ -2,20 +2,24 @@ import time
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from scipy.stats import zscore
+from sklearn import svm
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn import metrics, naive_bayes
+
 from utils.data_setup import DataSetup
 from utils.metrics import Metrics
 
 # Data retrieving
 data = DataSetup.data_setup(5)
 
+# Data preparation
 # Removing all duplicates
 data = data.drop_duplicates()
 
 # Shuffling rows of dataframe, done due to consecutive dataset entry being similar to each other
 print("Shuffling data")
 sampler = np.random.permutation(len(data))
+# i dati vengono indicizzati in base agli indici di sampler
 data = data.take(sampler)
 
 # dummy encode labels, store separately
@@ -38,7 +42,6 @@ data = StandardScaler().fit_transform(data)
 data = pd.DataFrame(data)
 
 
-
 # .values trasforma i dati in formato tabellare DataFrame in un array multidimensionale NumPy
 # training data for the neural net
 training_data = data.values
@@ -54,9 +57,7 @@ print("Validation - 80/20 split")
 x_training, x_testing, y_training, y_testing = train_test_split(training_data, labels, test_size=0.20, random_state=42)
 y_training = np.argmax(y_training, axis=1)
 
-
-# model = naive_bayes.BernoulliNB()
-model = naive_bayes.GaussianNB()
+model = svm.SVC(kernel='linear')
 
 start = time.time()
 model.fit(x_training, y_training)
@@ -66,7 +67,6 @@ print("\nTraining time: " + str(time.time() - start)[0:7] + "s")
 # predict genera le predizioni in output per i campioni in input
 prediction = model.predict(x_testing)
 print("Total time: " + str(time.time() - start)[0:7] + "s\n")
-
 
 # METRICS
 Metrics.metrics(y_testing, prediction, flag=1)

utils/metrics.py
@@ -10,9 +10,9 @@ labels = ['benign', 'gafgyt_combo', 'gafgyt_junk', 'gafgyt_scan', 'gafgyt_tcp',
 
 
 class Metrics:
-    def metrics(y_testing, prediction):
+    def metrics(y_testing, prediction, flag=0):
         # argmax restituisce gli indici dei valori massimi lungo un asse
-        if isinstance(prediction, pd.DataFrame):
+        if flag==0:
             prediction = np.argmax(prediction, axis=1)
         truth = np.argmax(y_testing, axis=1)
         accuracy_score = metrics.accuracy_score(truth, prediction)
@@ -26,9 +26,10 @@ class Metrics:
         print("Precision: " + "{:.2%}".format(float(precision_score)))
         print("Recall: " + "{:.2%}".format(float(recall_score)))
         print("F1: " + "{:.2%}".format(float(f1_score)))
-        # print("Confusion Matrix:")
+
+        print("\nConfusion Matrix generated")
         # print(confusion_matrix)
-        print("\nMultilabel Confusion Matrix:")
+        print("Multilabel Confusion Matrix:")
         print(multilabel_confusion_matrix)
 
         # Seaborn confusion matrix