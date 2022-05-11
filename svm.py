Added feature extraction PCA, feature selection by KBest and variance, now each plot generated has the associated model name, and added data_exploration.py to be used to solve dataset bug

svm.py
@@ -2,7 +2,6 @@ import time
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from scipy.stats import zscore
 from sklearn import svm
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
@@ -41,6 +40,10 @@ data = StandardScaler().fit_transform(data)
 # data = MinMaxScaler().fit_transform(data)
 data = pd.DataFrame(data)
 
+# Feature Selection
+# data = VarianceThreshold().fit_transform(data)
+# data = SelectKBest(chi2, k=100).fit_transform(data, labels_full)
+# data = pd.DataFrame(data)
 
 # .values trasforma i dati in formato tabellare DataFrame in un array multidimensionale NumPy
 # training data for the neural net
@@ -69,4 +72,4 @@ prediction = model.predict(x_testing)
 print("Total time: " + str(time.time() - start)[0:7] + "s\n")
 
 # METRICS
-Metrics.metrics(y_testing, prediction, flag=1)
+Metrics.metrics(y_testing, prediction, flag=1, name='SVC')

utils/metrics.py
@@ -10,7 +10,7 @@ labels = ['benign', 'gafgyt_combo', 'gafgyt_junk', 'gafgyt_scan', 'gafgyt_tcp',
 
 
 class Metrics:
-    def metrics(y_testing, prediction, flag=0):
+    def metrics(y_testing, prediction, name, flag=0):
         # argmax restituisce gli indici dei valori massimi lungo un asse
         if flag==0:
             prediction = np.argmax(prediction, axis=1)
@@ -36,7 +36,7 @@ class Metrics:
         confusion_matrix = pd.DataFrame(confusion_matrix, index=labels, columns=labels)
         plt.figure(figsize=(20, 10))
         seaborn.heatmap(confusion_matrix, annot=True)
-        plt.title('Confusion Matrix')
+        plt.title('Confusion Matrix '+name)
         plt.ylabel('Truth Values')
         plt.xlabel('Predicted Values')
         plt.show()